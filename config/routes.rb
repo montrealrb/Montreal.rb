09fc1966cc936a9f0978f3fde7a1f7a3b6674672
Nicholas Henry <nicholas.henry@gmail.com>
2015-11-23 18:00:03 -0500

    Replace RailsAdmin with Thoughtbot’s Adminstrative

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    DashboardManifest::DASHBOARDS.each do |dashboard_resource|
+      resources dashboard_resource
+    end
+
+    root controller: DashboardManifest::ROOT_DASHBOARD, action: :index
+  end
+
   devise_for :users
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   resources :events
 
