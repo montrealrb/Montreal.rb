7213be9486577298089af50871b8e926cfd8df0a
Pierre-Yves Poujade <py@poujade.org>
2015-11-24 21:43:11 -0500

    Add tests for member

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Member, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "without a name" do
+    it "should not create a member" do
+      member = Member.new
+
+      member.email = 'gala@example.com'
+      member.save
+
+      expect(member.valid?).to be false
+    end
+  end
+
+  context "without an email" do
+    it "should not create a member" do
+      member = Member.new
+
+      member.name = 'Zaba'
+      member.save
+
+      expect(member.valid?).to be false
+    end
+  end
+
+  context "with a duplicate email" do
+    it "should not create a member" do
+      member1 = Member.new
+      member1.name = 'Zaba'
+      member1.email = 'gala@example.com'
+
+      member2 = Member.new
+      member2.name = 'Gala'
+      member2.email = 'gala@example.com'
+
+      member1.save
+      member2.save
+
+      expect(member2.valid?).to be false
+    end
+  end
 end
