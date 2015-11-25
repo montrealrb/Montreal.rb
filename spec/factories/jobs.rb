8473821c9b207fc1da990fe0de9c714892dbca19
Anonyo Hossain <anonyo.hossain@koolicar.com>
2015-11-25 08:46:37 -0500

    Fixes indentation for jobs factory

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
   factory :job do
     state "MyString"
-title "MyString"
-description "MyText"
-organization_id 1
+    title "MyString"
+    description "MyText"
+    organization_id 1
   end
-
 end
