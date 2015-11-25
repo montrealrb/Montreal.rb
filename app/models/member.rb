c4914ceea528de12e810f40583c1bd7c23c28432
Pierre-Yves Poujade <py@poujade.org>
2015-11-24 21:11:30 -0500

    Validate presence of name, email and uniqueness of email

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,7 @@ class Member < ActiveRecord::Base
   has_many :events
   has_many :organizations
   has_one :user
+
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
 end
