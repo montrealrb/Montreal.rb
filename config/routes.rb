128b7b837b9a7db9f2c3f9ec66b1d92a2285c54f
Kira McLean <kiraemclean@gmail.com>
2015-11-24 21:07:50 -0500

    Remove index route for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :pages
+  resources :pages, except: [:index]
   namespace :admin do
     DashboardManifest::DASHBOARDS.each do |dashboard_resource|
       resources dashboard_resource
