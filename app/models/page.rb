7a5483c3b607a800a2dbdf05081a80080d2d25e5
Martin Carel <martin.carel@gmail.com>
2015-11-26 16:20:37 -0500

    Add basic attribute validations for the page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,9 @@
 class Page < ActiveRecord::Base
   extend Enumerize
 
+  validates :title, presence: true
+  validates :state, presence: true
+
   enumerize :state, in: [:draft, :published, :archived], default: :draft
 
   scope :published, -> { where(state: 'published') }
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Location, :type => :model do
+RSpec.describe Page, :type => :model do
+
+  describe 'attribute validations' do
+    it 'validates when the body is not defined' do
+      page = build :page, body: nil
+      expect(page).to be_valid
+    end
+
+    it 'does not validate when the title is not defined' do
+      page = build :page, title: nil
+      expect(page).to_not be_valid
+    end
+
+    it 'does not validate when the state is not defined' do
+      page = build :page, state: nil
+      expect(page).to_not be_valid
+    end
+  end
+
   it 'defaults to a draft state' do
     expect(Page.new.state).to eq 'draft'
   end
+
 end
