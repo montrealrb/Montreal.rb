8a98f901077084c7149684aefbb04498db058447
Martin Carel <martin.carel@gmail.com>
2015-11-27 01:33:25 -0500

    Make migrations globalize-compatible

--- db/migrate/20140929233719_create_events.rb
+++ db/migrate/20140929233719_create_events.rb
@@ -1,10 +1,16 @@
 class CreateEvents < ActiveRecord::Migration
-  def change
+  def up
     create_table :events do |t|
       t.string :type, null: false
       t.datetime :starts_at, null: false
       t.timestamps
     end
     Event.create_translation_table! :title => :string, :introduction => :text, :conclusion => :text
+  end
+
+  def down
+    drop_table :events do
+    end
+    Event.drop_translation_table!
   end
 end
