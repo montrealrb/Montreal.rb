07970dd380accbaec2c587e14454c7b77ea11b12
Eric M <ericmarchese77@gmail.com>
2015-11-30 18:45:41 -0500

    Change github key/secret placeholders to env variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,7 +234,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :github, 'GITHUB_KEY', 'GITHUB_SECRET', scope: 'user:email'
+  config.omniauth :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'],
+    scope: 'user:email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
