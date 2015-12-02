53ed1f697594f235ff98cfab269cc76d7ff93deb
itsWill <guilhermerpmansur@gmail.com>
2015-12-02 12:48:51 -0500

    minor style fix

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -2,6 +2,6 @@ class NewsItem < ActiveRecord::Base
   extend Enumerize
   enumerize :state, in: [:draft, :archived, :published]
   scope :published, -> { where(state: :published).order(:published_at) }
-  validates :published_at, presence: true, if: lambda{ state.published? } 
+  validates :published_at, presence: true, if: -> { state.published? } 
   validates_presence_of :title, :state
 end
