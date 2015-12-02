4fa3567a4b6a94000d7bf8832682e66dbe616d23
Martin Carel <martin.carel@gmail.com>
2015-12-02 10:26:26 -0500

    Markdown: move the html renderer into a TextHelper module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-  @@renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
-
-  def render_markdown_as_html(markup)
-    raw @@renderer.render(markup)
-  end
 end
---
+++ app/helpers/text_helper.rb
@@ -0,0 +1,7 @@
+module TextHelper
+  @@renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+
+  def render_markdown_as_html(markup)
+    raw(@@renderer.render(markup))
+  end
+end
