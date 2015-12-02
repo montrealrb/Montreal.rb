b8701b6c5e0b3362390771626ee78e88da2d72f8
Martin Carel <martin.carel@gmail.com>
2015-12-01 22:20:40 -0500

    Events: add spec regarding the presence of the 'starts_at' field

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -12,5 +12,13 @@
 require 'rails_helper'
 
 RSpec.describe Event, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'attribute validations' do
+    it 'does not validate when starts_at is not defined' do
+      event = Event.new(starts_at: nil)
+      expect(event).to_not be_valid
+      expect(event.errors.messages.keys).to include :starts_at
+    end
+  end
+
 end
