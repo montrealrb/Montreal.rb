1fb92b86ca4b2d2c6a0aa2c210e41c1559a22924
Martin Carel <martin.carel@gmail.com>
2015-12-03 16:50:32 -0500

    Events: make 'title' a required field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125023733) do
+ActiveRecord::Schema.define(version: 20151204172708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20151125023733) do
     t.string   "locale",       null: false
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.string   "title"
+    t.string   "title",        null: false
     t.text     "introduction"
     t.text     "conclusion"
   end
