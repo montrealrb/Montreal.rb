9c5f85174027b770424ba11357c4e4950e15011e
AndyObtiva <andy.am@gmail.com>
2015-12-04 18:27:19 -0500

    Display news item state enum form field as dropdown

--- app/dashboards/news_item_dashboard.rb
+++ app/dashboards/news_item_dashboard.rb
@@ -12,7 +12,7 @@ class NewsItemDashboard < Administrate::BaseDashboard
     id: Field::Number,
     title: Field::String,
     body: Field::Text,
-    state: Field::String,
+    state: EnumField,
     published_at: Field::DateTime,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
