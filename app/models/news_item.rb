16f4b599dd0346ebcf2238eade9ca1993fdbc581
AndyObtiva <andy.am@gmail.com>
2015-12-04 18:03:48 -0500

    https://github.com/montrealrb/Montreal.rb/issues/117 valid news item

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,7 +1,10 @@
 class NewsItem < ActiveRecord::Base
   extend Enumerize
-  enumerize :state, in: [:draft, :archived, :published]
+  AVAILABLE_STATES = %w(draft archived published)
+  enumerize :state, in: AVAILABLE_STATES
   scope :published, -> { where(state: :published).order(:published_at) }
-  validates :published_at, presence: true, if: -> { state.published? } 
-  validates_presence_of :title, :state
+  validates :published_at, presence: true, if: -> { state.try(:published?) }
+  validates :title, presence: true, length: {maximum: 255}
+  validates :state, presence: true, inclusion: {in: AVAILABLE_STATES, message: "must be one of the following: #{AVAILABLE_STATES.join(', ')}"}
+  validates :body, presence: true, length: {maximum: 4000}
 end
