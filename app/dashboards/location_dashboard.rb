90a5e642ef3f41c748cf574d67aab26923d11232
Martin Carel <martin.carel@gmail.com>
2015-12-05 17:22:20 -0500

    Locations: remove 'translation ids' field in admin interface

--- app/dashboards/location_dashboard.rb
+++ app/dashboards/location_dashboard.rb
@@ -37,7 +37,6 @@ class LocationDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    :translations,
     :name,
     :address,
     :url,
