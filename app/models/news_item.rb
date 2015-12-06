f54fac5092aed79530ed1d5de5288944d376d9f9
AndyObtiva <andy.am@gmail.com>
2015-12-05 20:11:14 -0500

    Add new max column length constants to news item via AR extension

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,10 +1,18 @@
 class NewsItem < ActiveRecord::Base
   extend Enumerize
-  AVAILABLE_STATES = %w(draft archived published)
-  enumerize :state, in: AVAILABLE_STATES
+  STATES = %w(draft published archived)
+  enumerize :state, in: STATES
   scope :published, -> { where(state: :published).order(:published_at) }
-  validates :published_at, presence: true, if: -> { state.try(:published?) }
-  validates :title, presence: true, length: { maximum: 255 }
-  validates :state, presence: true, inclusion: { in: AVAILABLE_STATES }
-  validates :body, presence: true, length: { maximum: 4000 }
+  validates :published_at,
+            presence: true,
+            if: -> { state.try(:published?) }
+  validates :title,
+            presence: true,
+            length: { maximum: MAX_STRING_COLUMN_LENGTH }
+  validates :body,
+            presence: true,
+            length: { maximum: MAX_TEXT_COLUMN_LENGTH }
+  validates :state,
+            presence: true,
+            inclusion: { in: STATES }
 end
--- spec/models/news_item_spec.rb
+++ spec/models/news_item_spec.rb
@@ -3,11 +3,17 @@ require "rails_helper"
 RSpec.describe NewsItem, type: :model do
   describe "validation" do
     it { should validate_presence_of(:title) }
-    it { should validate_length_of(:title).is_at_most(255) }
+    it do
+      should validate_length_of(:title).
+        is_at_most(ActiveRecordExtensions::MAX_STRING_COLUMN_LENGTH)
+    end
     it { should validate_presence_of(:body) }
-    it { should validate_length_of(:body).is_at_most(4000) }
+    it do
+      should validate_length_of(:body).
+        is_at_most(ActiveRecordExtensions::MAX_TEXT_COLUMN_LENGTH)
+    end
     context "for state" do
-      NewsItem::AVAILABLE_STATES.each do |state|
+      NewsItem::STATES.each do |state|
         it "passes for value #{state}" do
           news_item = build(:news_item, state: state)
           expect(news_item).to be_valid
