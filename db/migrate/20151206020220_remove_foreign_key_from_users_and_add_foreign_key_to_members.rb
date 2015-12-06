db2043eb73d3abd60d09fc9fdad11946a7702be3
Pierre-Yves Poujade <py@poujade.org>
2015-12-05 23:51:17 -0500

    Split migration in two

--- db/migrate/20151206020220_remove_foreign_key_from_users_and_add_foreign_key_to_members.rb
+++
@@ -1,9 +0,0 @@
-class RemoveForeignKeyFromUsersAndAddForeignKeyToMembers < ActiveRecord::Migration
-  def change
-    remove_foreign_key :users, :members
-    remove_column :users, :member_id
-
-    add_column :members, :user_id, :integer
-    add_foreign_key :members, :users
-  end
-end
