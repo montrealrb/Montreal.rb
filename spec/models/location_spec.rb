1285554a63957313f359ad2cce4ced650458951c
Henry Jenkins <henry@henryjenkins.name>
2016-02-22 16:32:55 -0500

    Added missing relationship

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Location, type: :model do
+  it { should have_many(:events) }
+
   describe "attribute validations" do
     it "does not validate when name is not defined" do
       location = Location.new(name: nil)
