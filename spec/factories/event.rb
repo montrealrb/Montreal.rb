bb945253d8ad6a697c646a85f3c3838007e1d4b1
Sophie Deziel <courrier@sophiedeziel.com>
2016-03-03 22:22:47 -0500

    deleted duplicated factories

--- spec/factories/event.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :event do
-    title { Faker::Book.title }
-    introduction { Faker::Lorem.sentence }
-    conclusion { Faker::Lorem.sentence }
-    starts_at 10.days.from_now
-    location
-  end
-end
