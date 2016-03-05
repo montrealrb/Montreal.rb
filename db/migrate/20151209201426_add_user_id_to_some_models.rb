02ae097d2bea202df4d7c4826d924d2813012a9b
Martin Carel <martin.carel@gmail.com>
2016-03-05 17:33:12 -0500

    db: make sure each row has a value before adding a non-null db constraint

--- db/migrate/20151209201426_add_user_id_to_some_models.rb
+++ db/migrate/20151209201426_add_user_id_to_some_models.rb
@@ -1,12 +1,19 @@
 class AddUserIdToSomeModels < ActiveRecord::Migration
   def up
-    add_column :events, :user_id, :integer, null: false
-    add_column :jobs, :user_id, :integer, null: false
-    add_column :news_items, :user_id, :integer
 
     User.create_default_user! if User.default_user.blank?
+
+    add_column :news_items, :user_id, :integer
     NewsItem.update_all user_id: User.default_user.id
     change_column_null :news_items, :user_id, false
+
+    add_column :jobs, :user_id, :integer
+    Job.update_all user_id: User.default_user.id
+    change_column_null :jobs, :user_id, false
+
+    add_column :events, :user_id, :integer
+    Event.update_all user_id: User.default_user.id
+    change_column_null :events, :user_id, false
 
     add_index :events, :user_id
     add_index :jobs, :user_id
