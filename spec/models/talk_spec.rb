c951ad949891bad39da3122e10d5121b3ccbd373
Sophie Deziel <courrier@sophiedeziel.com>
2016-03-09 19:42:56 -0500

    added some tests

--- spec/models/talk_spec.rb
+++ spec/models/talk_spec.rb
@@ -1,5 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Talk, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to :event }
+
+  describe "attributes validation" do
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :event }
+  end
 end
