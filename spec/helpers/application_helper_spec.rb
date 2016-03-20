f20d43deead63d683a1cf72a94fc88b3528bd494
Kira McLean <kiraemclean@gmail.com>
2016-03-20 11:22:57 -0400

    Add `full_title` helper to show meaningful page titles

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe 'full_title' do
+
+    context 'page has a title' do
+      it 'displays the full title' do
+        expect(full_title('Events')).to eq 'Events | Montreal.rb'
+      end
+    end
+
+    context 'page does not have a title' do
+      it 'displays the right title' do
+        expect(full_title(' ')).to eq 'Montreal.rb'
+      end
+    end
+  end
+end
