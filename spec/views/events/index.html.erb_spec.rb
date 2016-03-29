329b87be0e893cc6c8230270d169b8adb047f2a9
Kira McLean <kiraemclean@gmail.com>
2016-03-28 23:01:38 -0400

    Please the hound

--- spec/views/events/index.html.erb_spec.rb
+++ spec/views/events/index.html.erb_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "events/index.html.erb" do
   let!(:future_event) { create :event, :scheduled, starts_at: 1.month.from_now }
-  let!(:past_event)   { create :event, :scheduled, starts_at: 1.month.ago}
+  let!(:past_event)   { create :event, :scheduled, starts_at: 1.month.ago }
 
   before do
     assign :future_events, [future_event]
@@ -20,22 +20,24 @@ describe "events/index.html.erb" do
     expect(rendered).to have_link future_event.title.titleize, href: event_path(future_event)
   end
 
-  context 'event has a talk' do
+  context "event has a talk" do
     let!(:member) { create :member }
-    let!(:talk)   { create :talk, state: 'scheduled', event_id: past_event.id, member_id: member.id }
+    let!(:talk)   { create :talk, state: "scheduled",
+                                  event_id: past_event.id,
+                                  member_id: member.id }
 
-    it 'displays the talks header' do
+    it "displays the talks header" do
       render
-      expect(rendered).to have_css 'h3', text: 'Talks'
-      expect(rendered).to have_css 'h4', text: "#{talk.title}, by #{member.name}"
+      expect(rendered).to have_css "h3", text: "Talks"
+      expect(rendered).to have_css "h4", text: "#{talk.title}, by #{member.name}"
     end
   end
 
-  context 'event has no talks' do
-    it 'does not show anything about talks' do
+  context "event has no talks" do
+    it "does not show anything about talks" do
       render
-      expect(rendered).not_to have_css 'h3', text: 'Talks'
-      expect(rendered).not_to have_css 'h4'
+      expect(rendered).not_to have_css "h3", text: "Talks"
+      expect(rendered).not_to have_css "h4"
     end
   end
 end
