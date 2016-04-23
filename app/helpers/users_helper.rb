c4999ab1576fcb975caa9b8154ae0c069d7cd8f0
Sophie Deziel <courrier@sophiedeziel.com>
2016-04-22 20:04:36 -0400

    closes #221

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,7 @@
 module UsersHelper
-  def gravatar_for(user, options = { size: 200 })
-    size = options[:size]
+  def gravatar_for(user, size: 200, html: {})
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar img-circle", width: '100%')
+    image_tag(gravatar_url, html.merge(alt: user.name))
   end
 end
