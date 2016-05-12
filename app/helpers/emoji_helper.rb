7c153d47ce37728a02bd88965fdffc6164c7f3b9
itsWill <guilhermerpmansur@gmail.com>
2016-05-12 22:50:06 +0200

    make hound happier

--- app/helpers/emoji_helper.rb
+++ app/helpers/emoji_helper.rb
@@ -1,7 +1,7 @@
 module EmojiHelper
   def emojify(content)
     h(content).to_str.gsub(/:([\w+\s-]+):/) do |match|
-      name = $1.tr(' ', '_').downcase
+      name = $1.tr(" ", "_").downcase
       if emoji = Emoji.find_by_alias(name)
         %(<img alt="#{name}" src="#{image_path("emoji/#{emoji.image_filename}")}" style="vertical-align:middle" width="20" height="20" />)
       else
