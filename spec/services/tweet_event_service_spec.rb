1128d79ee9c227360e6993575bfe46a3d4c6f279
Sophie Deziel <courrier@sophiedeziel.com>
2016-05-16 19:26:36 -0400

    added tests to get back to 100% coverage

--- spec/services/tweet_event_service_spec.rb
+++ spec/services/tweet_event_service_spec.rb
@@ -2,17 +2,20 @@ require "rails_helper"
 
 RSpec.describe TweetEventService do
   let(:event) { create(:event) }
-  let(:message) { "Testing the twitter feature for montrealrb.com" }
   let(:service) { described_class.new(event) }
 
   subject { service.tap(&:call) }
 
-  before { allow(service).to receive(:message) { message } }
-
   its(:success?) { is_expected.to eq true }
 
   it "tweets" do
-    allow_any_instance_of(Twitter::REST::Client).to receive(:update).with(message)
+    allow_any_instance_of(Twitter::REST::Client).to receive(:update).with(String)
     subject
+  end
+
+  context "when twitter returns an error" do
+    before { allow_any_instance_of(Twitter::REST::Client).to receive(:update).and_raise(Twitter::Error) }
+
+    its(:success?) { is_expected.to eq false }
   end
 end
