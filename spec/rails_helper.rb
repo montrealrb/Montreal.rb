af8ad63a9257eb47eb90eb5f45ccfaa645e7d0f6
Sophie Deziel <courrier@sophiedeziel.com>
2016-05-16 19:16:39 -0400

    fix deprecation warning

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,13 +11,13 @@ require "vcr"
 require "webmock/rspec"
 WebMock.disable_net_connect!(allow_localhost: true)
 
-VCR.config do |config|
+VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.filter_sensitive_data("<TWITTER_CONSUMER_KEY>") { ENV["TWITTER_CONSUMER_KEY"] }
+  config.filter_sensitive_data("<TWITTER_CONSUMER_KEY>")    { ENV["TWITTER_CONSUMER_KEY"]    }
   config.filter_sensitive_data("<TWITTER_CONSUMER_SECRET>") { ENV["TWITTER_CONSUMER_SECRET"] }
-  config.filter_sensitive_data("<TWITTER_ACCESS_TOKEN>") { ENV["TWITTER_ACCESS_TOKEN"] }
-  config.filter_sensitive_data("<TWITTER_ACCESS_SECRET>") { ENV["TWITTER_ACCESS_SECRET"] }
+  config.filter_sensitive_data("<TWITTER_ACCESS_TOKEN>")    { ENV["TWITTER_ACCESS_TOKEN"]    }
+  config.filter_sensitive_data("<TWITTER_ACCESS_SECRET>")   { ENV["TWITTER_ACCESS_SECRET"]   }
 end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
