517a978131cee5a7cdd47034df88e13f4c115cd0
Charles Lalonde <charles@crew.co>
2016-05-18 00:32:46 -0400

    Fix N+1 query errors
    New gem (Bullet) to catch all the N+1 queries.
    Also, fix the N+1 in all the user's facing controllers.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,11 @@
 class EventsController < ApplicationController
   def index
-    @past_events, @future_events = Event.published.partition { |e| e.starts_at < Time.now }
+    @past_events, @future_events = Event.published.includes(
+      :translations,
+      :location,
+      :talks,
+      :sponsors
+    ).partition { |e| e.starts_at < Time.now }
   end
 
   def show
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @past_events, @future_events = Event.published.partition { |e| e.starts_at < Time.now }
+    @past_events, @future_events = Event.published.includes(
+      :translations,
+      :location,
+      :talks,
+      :sponsors
+    ).partition { |e| e.starts_at < Time.now }
   end
 end
