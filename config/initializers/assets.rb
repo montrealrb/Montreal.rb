7138d2b280eb2c5764f38a56e6e59179f66240fe
Sophie Deziel <courrier@sophiedeziel.com>
2016-12-20 21:18:31 -0500

    remove administrate assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += ["joblint.min.js"]
+Rails.application.config.assets.precompile += ["joblint.min.js", 'administrate/job-show.css']
