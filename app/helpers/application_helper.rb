ccd2b24805b3526e2ec348f27fc24d4e6dd88b58
Sophie Deziel <courrier@sophiedeziel.com>
2017-03-23 22:48:08 -0400

    implement and test the service

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,32 +10,4 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
-
-  def email_slack_invite(user_email)
-    # Get the channel ID from https://slack.com/api/channels.list?token=<YOUR_TOKEN>
-    channels = ""
-
-    # Get the auth token from: https://api.slack.com/web
-    token = ""
-
-    # Construct the API request URL
-    # This is not documented by Slack, and may not work forever
-    # As it is an unofficial API endpoint, use at your own risk!
-    url = "https://slack.com/api/users.admin.invite"
-    url += "?channels=#{channels}"
-    url += "&set_active=true"
-    url += "&_attempts=1"
-    url += "&token=#{token}"
-    url += "&email=#{URI.escape(user_email)}"
-
-    # Make the request and capture the response
-    response = JSON.parse(open(url).read)
-
-    if response["ok"]
-      Rails.logger.info("slack invitation sent to: #{user_email}".colorize(:green))
-    else
-      Rails.logger.info("slack invitation failed to send to #{user_email}".colorize(:red))
-      Rails.logger.info(response.inspect.colorize(:yellow))
-    end
-  end
 end
---
+++ spec/services/slack_invitation_sending_service_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe SlackInvitationSendingService do
+  let(:user) { build :user, email: 'courrier+new@sophiedeziel.com' }
+
+  subject { described_class.new(user) }
+
+  describe '#call', skip: ENV['SLACK_TOKEN'].nil? do
+    it 'is success', record: true do
+      expect(subject.call).to be_success
+    end
+
+    it 'logs it', record: true do
+      expect(Rails.logger).to receive(:info)
+      subject.call
+    end
+
+    context 'when there is an error' do
+      subject { described_class.new(user, 'sadfasdfsdf') }
+
+      it 'is not success', record: true do
+        expect(subject.call).to_not be_success
+      end
+
+      it 'logs it', record: true do
+        expect(Rails.logger).to receive(:info).twice
+        subject.call
+      end
+    end
+  end
+end
