a6ac70e612a2dc11b112f90b4d995343ab1a1c70
Charles Lalonde <charles.lalonde15@gmail.com>
2017-05-24 16:18:56 -0400

    Create jobs migration (#271)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523233820) do
+ActiveRecord::Schema.define(version: 20170524022609) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20170523233820) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "user_id",         null: false
+    t.datetime "published_at"
   end
 
   add_index "jobs", ["user_id"], name: "index_jobs_on_user_id"
