0961a4f8a4ab8494621cdd1cd63a86d65843bc2b
François Belle <bellef@users.noreply.github.com>
2018-02-21 22:18:08 -0500

    Feature/searchable jobs (#268)
    * Added search input field in jobs index
    
    * Filter jobs with query input before rendering index
    
    * jobs_controller_spec update to test search query
    
    * Double quotes instead of single ones
    
    * Rework after xymbol feedback:
    - Move search logic to Job model
    - Replace "/jobs" with jobs_path
    - Change specs accordingly
    
    * Shorter lines
    
    * Use let and subject instead of variables and hard coded Model name
    
    * Scope instead of class method 'search'
    
    * Better specs: use 'subject' properly ; use implicit form of 'it'
    
    * Hash condition on scope. Add 'published' to 'search' scope
    
    * 'published' scope at the right place
    
    * We search a 'query', not a 'search'
    
    * Use arel table to build SQL query
    
    * Simplify the scope

--- app/models/job.rb
+++ app/models/job.rb
@@ -10,6 +10,7 @@ class Job < ActiveRecord::Base
   belongs_to :organization
 
   scope :published, -> { where(state: :published).order(created_at: :desc) }
+  scope :search, -> (query = nil) { where("title LIKE ? OR description LIKE ?", "%#{query}%", "%#{query}%") if query }
   scope :draft, -> { where(state: :draft) }
 
   enumerize :state, in: STATES, default: :draft
